b70d2e33b16431957c91837593576a6fe13d8d07
Ben Nortier <bjnortier@gmail.com>
2016-01-26 12:32:35 +0200

    Triangulate 3D

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var path = require('path');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var jscsStylish = require('gulp-jscs-stylish');
+
+var srcFiles = path.join('lib', '**', '*.js');
+var unitTestFiles = path.join('test', 'unit', '**', '*.test.js');
+var functionalTestFiles = path.join('test', 'functional', '**', '*.js');
+
+gulp.task('clearconsole', function() {
+  process.stdout.write('\x1Bc');
+});
+
+gulp.task('jshint', function() {
+  return gulp.src([
+    srcFiles,
+    unitTestFiles,
+    functionalTestFiles
+  ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  return gulp.src([
+    srcFiles,
+    unitTestFiles,
+    functionalTestFiles
+  ])
+    .pipe(jscs())
+    .pipe(jscsStylish());
+});
+
+gulp.task('unit', ['jshint', 'jscs'], function() {
+  return gulp.src(unitTestFiles)
+    .pipe(mocha());
+});
+
+gulp.task('functional', ['unit'], function() {
+
+});
+
+gulp.task('test', ['jshint', 'jscs', 'unit', 'functional']);
+
+gulp.task('default', ['test']);
+
+gulp.task('watch', function() {
+  gulp.watch([srcFiles], ['clearconsole', 'jshint', 'jscs', 'unit', 'functional']);
+  gulp.watch([unitTestFiles], ['clearconsole', 'jshint', 'jscs', 'unit']);
+  gulp.watch([functionalTestFiles], ['clearconsole', 'jshint', 'jscs', 'functional']);
+});
